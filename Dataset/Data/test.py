2499669ce0536bcdc89ee2e930a31a7f9882210c
aarnol <114937598+aarnol@users.noreply.github.com>
2024-12-16 11:33:01 -0700

    commit before removing

--- Dataset/Data/test.py
+++ Dataset/Data/test.py
@@ -8,15 +8,5 @@ import fnirs_utils
 # Define the path
 fnirs_data, MNI = fnirs_utils.load_fnirs(os.path.join(os.path.dirname(__file__), 'fNIRS'))
 
-data = torch.load(os.path.join('Dataset\Data\dataset_hcpWM_sphere.save'))
-
-
-
-
-print(np.array(data[-1]['pheno']['modality']))
-# Discard the fnirs modality data
-new_data = [d for d in data if d['pheno']['modality'] != 'fNIRS']
-print(np.array(new_data[-1]['pheno']['modality']))
-check = [d for d in new_data if d['pheno']['modality'] == 'fNIRS']
-print(len(check))
-torch.save(new_data, os.path.join('Dataset\Data\dataset_hcpWM_sphere_mod.save'))
+data = torch.load(os.path.join('Dataset\Data\dataset_hcpWM_sphere_orig_MNI.save'))
+print(data[0]['roiTimeseries'].shape)
