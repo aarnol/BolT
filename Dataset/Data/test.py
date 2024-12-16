7fc02941a6f5272a5498b283c35c78b3df0c8013
aarnol <114937598+aarnol@users.noreply.github.com>
2024-12-16 12:08:23 -0700

    commit before rollback testing

--- Dataset/Data/test.py
+++ Dataset/Data/test.py
@@ -7,7 +7,6 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'Prep'))
 import fnirs_utils
 # Define the path
 fnirs_data, MNI = fnirs_utils.load_fnirs(os.path.join(os.path.dirname(__file__), 'fNIRS'))
-print(fnirs_data)
-data = torch.load(os.path.join('Dataset\Data\dataset_hcp_schaefer7_400_fNIRS_1.save'))
-data += fnirs_data
-print(data)
+
+data = torch.load(os.path.join('Dataset\Data\dataset_hcpWM_sphere_orig_MNI.save'))
+print(data[0]['roiTimeseries'].shape)
