644ccb45fc6b72a08c726e23972f94d02f39b5ff
aarnol <114937598+aarnol@users.noreply.github.com>
2025-01-06 08:29:33 -0700

    testing AAL

--- Dataset/Data/test.py
+++ Dataset/Data/test.py
@@ -6,7 +6,7 @@ import torch
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'Prep'))
 import fnirs_utils
 # Define the path
-fnirs_data, MNI = fnirs_utils.load_fnirs(os.path.join(os.path.dirname(__file__), 'fNIRS'))
+# fnirs_data, MNI = fnirs_utils.load_fnirs(os.path.join(os.path.dirname(__file__), 'fNIRS'))
 
-data = torch.load(os.path.join('Dataset\Data\dataset_hcpWM_sphere_orig_MNI.save'))
-print(data[0]['roiTimeseries'].shape)
+data = torch.load(os.path.join('Dataset\Data\dataset_hcp_AAL_fNIRS_5.save'))
+print(data[0]['roiTimeseries'][0].shape)
