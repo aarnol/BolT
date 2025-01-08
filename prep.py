a93d61116d99f08b357933b017c0c972aadc253a
aarnol <alar6830@colorado.edu>
2025-01-08 14:20:03 -0700

    updated git ignore

--- prep.py
+++ prep.py
@@ -9,6 +9,8 @@ parser.add_argument("-d", "--dataset", type=str, choices=["abide1", "hcpRest", "
 parser.add_argument("-a", "--atlas", type=str, choices=["schaefer7_400", "AAL", "sphere", "brodmann"], default="schaefer7_400")
 parser.add_argument("-f", "--fnirs", type=bool, default=False)
 parser.add_argument("-n", '--name', type = str)
+parser.add_argument("-r", "--radius", type= int, default = 30)
+parser.add_argument("-s", "--smoothing", type= int, default = None)
 argv = parser.parse_args()
 
 
@@ -21,6 +23,6 @@ elif(argv.dataset == "hcpWM"):
     prep = prep_hcp
 
 
-prep(argv.atlas, argv.name, argv.fnirs )
+prep(argv.atlas, argv.name, argv.fnirs , argv.radius, argv.smoothing)
 
 
